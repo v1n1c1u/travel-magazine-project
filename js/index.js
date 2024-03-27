96de34c5b38c51e45818338498f51b1754dbb994
v1n1c1u <vinicius.abner1039@gmail.com>
2024-03-27 16:41:34 -0700

    Adding svgs to contact page as well as fixing major responsiveness problems

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const toggleMenuButton = document.getElementById("toggle-button");
 const menu = document.getElementById("menu");
 
-toggleMenuButton.addEventListener("click", toggleMenu);
+if(toggleMenuButton) toggleMenuButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
     menu.classList.toggle("show-menu");
