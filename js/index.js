bc1e962ebbeb24d27cba7553b5caffdb31c4d489
v1n1c1u <vinicius.abner1039@gmail.com>
2024-03-28 19:30:13 -0700

    changing the colours of the navbarand adding the search icon for the light background

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 const toggleMenuButton = document.getElementById("toggle-button");
 const menu = document.getElementById("menu");
+const nav = document.getElementsByTagName("nav")[0];
 
 if(toggleMenuButton) toggleMenuButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
+    console.log("is working");
     menu.classList.toggle("show-menu");
+    if(menu.classList.contains("show-menu")){
+        nav.style.backgroundColor = "rgba(4, 19, 42, 0.95)";
+    }else{
+        nav.style.backgroundColor = "transparent";
+    }
 }
