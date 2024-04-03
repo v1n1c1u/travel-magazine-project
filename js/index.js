45813245bcbc430dc940d5b38e5ad130e3e9bbee
v1n1c1u <vinicius.abner1039@gmail.com>
2024-04-02 17:23:10 -0700

    Changing navbar colours in mobile and adding logic to footer form

--- js/index.js
+++ js/index.js
@@ -1,9 +1,35 @@
-const toggleMenuButton = document.getElementById("toggle-button");
-const menu = document.getElementById("menu");
+document.addEventListener("DOMContentLoaded",()=>{
+    const toggleMenuButton = document.getElementById("toggle-button");
+    const menu = document.getElementById("menu");
+    const newsletterForm = document.getElementById("subscribe-form");
+    const subscribeEmailInput = document.getElementById("subscription-email");
+    const footerErrorMessage = document.getElementById("error-message");
 
-if(toggleMenuButton) toggleMenuButton.addEventListener("click", toggleMenu);
+    toggleMenuButton.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
-    console.log("is working");
-    menu.classList.toggle("show-menu");
-}
+    function toggleMenu(){
+        console.log("is working");
+        menu.classList.toggle("show-menu");
+    }
+    //email validation based on: https://www.edureka.co/blog/javascript-email-validation/
+    newsletterForm.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const mailRegex = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+        if(subscribeEmailInput.value.match(mailRegex)){
+            subscribeEmailInput.classList.remove("invalid-input");
+            if(footerErrorMessage.classList.contains("visible")){
+                footerErrorMessage.classList.add("invisible");
+                footerErrorMessage.setAttribute("aria-hidden", "true");
+            }
+            
+            alert("You have entered a valid email address!");    //The pop up alert for a valid email address
+        }else{
+            footerErrorMessage.classList.add("visible");
+            footerErrorMessage.setAttribute("aria-hidden", "false");
+            subscribeEmailInput.classList.add("invalid-input");    //The pop up alert for an invalid email address
+            subscribeEmailInput.focus();   //The pop up alert for an invalid email address
+            document.wai
+            alert("You have entered an invalid email address!");
+        }
+    })
+})
